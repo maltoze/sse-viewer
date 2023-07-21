f62ef561534697fb1685acb8dc69b0ef5406c6bf
maltoze <atonezzz@gmail.com>
2023-07-21 12:13:48 +0800

    fix: send message to attached tab

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,9 @@
+let esUrl
+let sseRequestId
+let attachedTabId
+const esPathPrefix = '/__maltoze-sse-viewer'
+const bodyQueue = []
+
 function sendCommand(target, method, params) {
   return new Promise((resolve, reject) => {
     chrome.debugger.sendCommand(target, method, params, function (result) {
@@ -12,8 +18,9 @@ function sendCommand(target, method, params) {
 
 chrome.action.onClicked.addListener(function (tab) {
   if (tab.url.startsWith('http')) {
-    chrome.debugger.attach({ tabId: tab.id }, '1.2', function () {
-      sendCommand({ tabId: tab.id }, 'Fetch.enable', {
+    attachedTabId = tab.id
+    chrome.debugger.attach({ tabId: attachedTabId }, '1.2', function () {
+      sendCommand({ tabId: attachedTabId }, 'Fetch.enable', {
         patterns: [{ requestStage: 'Request' }, { requestStage: 'Response' }],
       })
     })
@@ -22,16 +29,7 @@ chrome.action.onClicked.addListener(function (tab) {
   }
 })
 
-const esPathPrefix = '/__maltoze-sse-viewer'
-let esUrl
-let sseRequestId
-const bodyQueue = []
-
 chrome.debugger.onEvent.addListener(async function (source, method, params) {
-  const [tab] = await chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true,
-  })
   if (method === 'Fetch.requestPaused') {
     const reqUrl = new URL(params.request.url)
     if (reqUrl.pathname === esUrl) {
@@ -44,7 +42,7 @@ chrome.debugger.onEvent.addListener(async function (source, method, params) {
         ],
         body: bodyQueue.shift(),
       })
-      chrome.tabs.sendMessage(tab.id, { type: 'close' })
+      chrome.tabs.sendMessage(attachedTabId, { type: 'close' })
     } else {
       if (params.request.headers.accept === 'text/event-stream') {
         sseRequestId = params.requestId
@@ -54,7 +52,7 @@ chrome.debugger.onEvent.addListener(async function (source, method, params) {
             requestId: params.requestId,
           })
           bodyQueue.push(resp.body)
-          chrome.tabs.sendMessage(tab.id, { type: 'create', url: esUrl })
+          chrome.tabs.sendMessage(attachedTabId, { type: 'create', url: esUrl })
         }
         sendCommand(source, 'Fetch.continueRequest', {
           requestId: params.requestId,
